cac18ea20a1b9a66c8c28946840b8e28f1d34a07
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2022-04-13 17:01:48 +0600

    do some initial design

--- frontend/src/component/layout/Home/Home.js
+++ frontend/src/component/layout/Home/Home.js
@@ -1,6 +1,18 @@
 import React from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
+import Product from "./Product";
+
+const product = {
+  name: "Blue T-shirt",
+  price: "$35",
+  images: [
+    {
+      url: "https://images-platform.99static.com//jGLR033kyVvlBCc142SOdkwSTUI=/0x49:1067x1116/fit-in/500x500/99designs-contests-attachments/70/70288/attachment_70288156",
+    },
+  ],
+  _id: "1",
+};
 
 const Home = () => {
   return (
@@ -13,6 +25,18 @@ const Home = () => {
             Scroll <CgMouse />
           </button>
         </a>
+      </div>
+      <h2 className="homeHeading">Feature Products</h2>
+      <div id="container" className="container">
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
       </div>
     </>
   );
